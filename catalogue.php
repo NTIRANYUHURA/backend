e1fe41a8f9907cc2d12c93c34c3003ce8c8fe5be
NTIRANYUHURA <floribertnt@gmail.com>
2023-01-11 10:09:54 +0100

    second commit

--- catalogue.php
+++ catalogue.php
@@ -3,16 +3,15 @@ require 'config/init.php';
 $tousProduits = $db->query('SELECT * FROM produit ORDER BY id DESC');
 if(isset($_GET['search']) AND !empty($_GET['search'])){
     $recherche =htmlspecialchars($_GET['search']);
-    $tousProduits=$db->query('SELECT * FROM produit WHERE titre LIKE "%'.$recherche.'%" or prix LIKE "%'.$recherche.'%" or description LIKE "%'.$recherche.'%" or image LIKE "%'.$recherche.'%"ORDER BY id DESC');
+    $sql ='SELECT * FROM produit WHERE titre LIKE "%'.$recherche.'%" or prix LIKE "%'.$recherche.'%" or description LIKE "%'.$recherche.'%" or image LIKE "%'.$recherche.'%"ORDER BY id DESC';
+    $tousProduits=$db->query($sql);
     $tousProduits->execute();
-    
-
 }
 ?>
 <h1>Rechercher des produits</h1>
 <form action = "catalogue.php" method = "GET" >
-    <input type = "search" name="search" placeholder= "rechercher un produit">
-    <input type = "submit" name="voir" value= "voir">
+    <input type = "search" name="search" placeholder="rechercher un produit">
+    <input type = "submit" name="voir" value="voir">
 </form>
 <?php
 include 'components/cardCatalogue.php';
@@ -20,28 +19,7 @@ $rows = $tousProduits->fetchAll(PDO::FETCH_ASSOC);
 foreach($rows as $p){
     cardCatalogue($p['image'],$p['titre'], $p['prix'], $p['description'],$p['id'] );
 }
-
-/*while($product=$tousProduits->fetch()){
-    ?>
-    <td>
-   <img src = "<?php echo $product['image']; ?> "/>
-   </td>
-   <tr>
-        <td><?php echo $product['titre']; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo $product['prix']; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo $product['description']; ?></td>
-    </tr>
-    <?php
-}
-
-*/
 ?>
-
-
 <?php
 include 'includes/footer.html'
 ?>
